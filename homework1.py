f6ba104c54cb6dc05aa31800785bbdbaf32cd6c6
Sabir587 <sabir250298@yandex.ru>
2024-07-29 15:30:23 +0300

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,14 @@
+name = 'Sabir'
+print(name [::-1])
+print (name [0:3])
+print (name [0:3:2])
+print (name [0:2])
+print (name [:1])
+print (name [0:5])
+print (name [2:])
+print (name [-1:])
+name = 'Urban'
+print (name [:1])
+print (name [4:])
+print (name [2:])
+
