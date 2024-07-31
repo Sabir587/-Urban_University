42f51603d08996f079219920acddccfaeec3f852
Sabir587 <sabir250298@yandex.ru>
2024-07-31 15:32:08 +0300

    Add files via upload
    Исправил

---
+++ module_1_1.py
@@ -0,0 +1,15 @@
+name = 'Sabir'
+print (name [:1])
+print (name [-1:])
+print (name [0:5])
+name = 'Urban'
+print (name [2:])
+print (name [::-1])
+print (name [:1])
+print (name [4:])
+example = 'топинамбур'
+print (example [:1])
+print (example [-1:])
+print (example [5:10])
+print (example [::-1])
+print(example [1:10:2])
