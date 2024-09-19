2c1b0957ac0beb85605a4caac0a810d2633d7b1e
Sabir587 <sabir250298@yandex.ru>
2024-09-19 17:21:18 +0300

    module_1_3.py

---
+++ module_1_3.py
@@ -0,0 +1,12 @@
+name = 'Sabir'
+print (name, type(name))
+age = 26
+print (age, type(age))
+new_age = '26'
+print ('age + new_age')
+is_student = 'True'
+print (is_student)
+a = 15
+print (a)
+a = 17
+print (a)
