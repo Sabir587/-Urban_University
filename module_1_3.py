804d5abbfe0244f3341d584eb0156befc30e2906
Sabir587 <sabir250298@yandex.ru>
2024-09-21 21:12:46 +0300

    module_1_3.1.py
    Исправлено

--- module_1_3.py
+++ module_1_3.py
@@ -2,8 +2,8 @@ name = 'Sabir'
 print (name, type(name))
 age = 26
 print (age, type(age))
-new_age = '26'
-print ('age + new_age')
+age = '26'
+print ('age + 26')
 is_student = 'True'
 print (is_student)
 a = 15
