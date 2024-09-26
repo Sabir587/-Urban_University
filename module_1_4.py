68f1727d66e58d6b96c486537bfcaebf4e8d1db4
Sabir587 <sabir250298@yandex.ru>
2024-09-26 23:33:59 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+# name = input("Введите ваше имя: ")
+# current_year = 2024
+# date_of_birth = int(input("дата вашего рождения?"))
+# age = current_year - date_of_birth
+# print ("Добрый день,", name)
+# print ("Вам исполнилось", age, "лет")
+print('моя, строка в нижнем регистре'.upper().lover())
+print ('моя, строка в нижнем регистре').replase(_old: 'здравствуйте', _new:'до свидание'))
