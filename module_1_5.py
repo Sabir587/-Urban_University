57f8cfe3267d5c0a19045474fdc9f2a4c1e984c8
Sabir587 <sabir250298@yandex.ru>
2024-10-15 15:36:46 +0300

    module_1_5.ру

---
+++ module_1_5.py
@@ -0,0 +1,9 @@
+immutable_var = tuple(3, 5, 'a', 'b',)
+print (immutable_var[1:5])
+print (immutable_var)
+immutable_var = (3, 5, 'a','b', 'modifiled')
+print (immutable_var[1:5])
+mutable_list = [8, 4, 'b', 'c,']
+print (mutable_list[1:5])
+mutable_list = [8, 4, 'b', 'c', 'modifiled']
+print (mutable_list[1:5])
